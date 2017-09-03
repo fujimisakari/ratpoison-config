5b15629fcf9fea70be2baf53ac0fdd3de14b60fe
Fujimisakari <fujimisakari@gmail.com>
2017-09-03 12:32:11 +0900

    update ratpoison command

--- cmds/select_windows.py
+++ cmds/select_windows.py
@@ -4,12 +4,9 @@
 from commands import getoutput
 from os import system
 
-fg_col = 'Orange'
-bg_col = '#1A1A1A'
-
 ratmenu_cmd = ['ratmenu']
-ratmenu_cmd.append('-fg "{}"'.format(fg_col))
-ratmenu_cmd.append('-bg "{}"'.format(bg_col))
+ratmenu_cmd.append('-fg "Orange"')
+ratmenu_cmd.append('-bg "#1A1A1A"')
 ratmenu_cmd.append('-align "left"')
 ratmenu_cmd.append('-style dreary')
 ratmenu_cmd.append('-label "select windows"')
