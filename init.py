9db2746c5fb32405b03ef1d9918ea45390cd9c62
Fujimisakari <fujimisakari@gmail.com>
2017-09-12 03:26:09 +0900

    add gtkrc

--- init.py
+++ init.py
@@ -3,7 +3,7 @@
 
 from os import system
 
-DOTFILES_LIST = ['.xkb', '.conkyrc', '.ratpoisonrc', '.Xresources', '.xremaprc']
+DOTFILES_LIST = ['.xkb', '.conkyrc', '.ratpoisonrc', '.Xresources', '.xremaprc', '.gtkrc-2.0']
 
 for dotfile in DOTFILES_LIST:
     system('ln -sf ~/.ratpoison-config/{} ~/.'.format(dotfile))
